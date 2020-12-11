90245f1a2ec0b93858314ab835f109c0d8169d88
Michael Jeffrey <5944277+michaeljeffrey@users.noreply.github.com>
2020-12-11 14:28:37 -0800

    refactor isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,10 +1,10 @@
 def is_isogram(phrase):
     
     char_set = set()
-    for char in phrase:
+    for char in phrase.lower():
         if char.isalpha():
-            if char.lower() not in char_set:
-                char_set.add(char.lower())
+            if char not in char_set:
+                char_set.add(char)
             else:
                 return False
     
