01eb4489b904fec45cd5aac2b0b69582e01c218e
Michael Jeffrey <5944277+michaeljeffrey@users.noreply.github.com>
2021-01-17 20:58:49 -0800

    twelve_days.py requested edits

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,19 +1,20 @@
-def recite(start_verse, end_verse):
+days = [  # list of days and gifts
+    ("first", "a Partridge in a Pear Tree."),
+    ("second", "two Turtle Doves, "),
+    ("third", "three French Hens, "),
+    ("fourth", "four Calling Birds, "),
+    ("fifth", "five Gold Rings, "),
+    ("sixth", "six Geese-a-Laying, "),
+    ("seventh", "seven Swans-a-Swimming, "),
+    ("eighth", "eight Maids-a-Milking, "),
+    ("ninth", "nine Ladies Dancing, "),
+    ("tenth", "ten Lords-a-Leaping, "),
+    ("eleventh", "eleven Pipers Piping, "),
+    ("twelfth", "twelve Drummers Drumming, ")
+]
 
-    days = [  # list of days and gifts
-        ("first", "a Partridge in a Pear Tree."),
-        ("second", "two Turtle Doves, "),
-        ("third", "three French Hens, "),
-        ("fourth", "four Calling Birds, "),
-        ("fifth", "five Gold Rings, "),
-        ("sixth", "six Geese-a-Laying, "),
-        ("seventh", "seven Swans-a-Swimming, "),
-        ("eighth", "eight Maids-a-Milking, "),
-        ("ninth", "nine Ladies Dancing, "),
-        ("tenth", "ten Lords-a-Leaping, "),
-        ("eleventh", "eleven Pipers Piping, "),
-        ("twelfth", "twelve Drummers Drumming, ")
-    ]
+
+def recite(start_verse, end_verse):
 
     song = []
     
@@ -30,8 +31,8 @@ def recite(start_verse, end_verse):
             and_word = ""
             if day == 0 and start_verse != 1:
                 and_word = "and "
-                
-            line = line + f"{and_word}" + f"{days[day][1]}"  # combine the line of gifts
+            
+            line = "".join([line, f"{and_word}", f"{days[day][1]}"])
             day -= 1
             
         song.append(line)
